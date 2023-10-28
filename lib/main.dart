2616ae053413708040ddb77167e8353b512a8f5f
khanian <khanylee@gmail.com>
2023-10-29 08:33:41 +0900

    widget stack badge

--- lib/main.dart
+++ lib/main.dart
@@ -25,29 +25,35 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Stack(
       children: [
-        Container(
-          width: 500,
-          height: 500,
-          color: Colors.black,
-        ),
-        Container(
-          width: 400,
-          height: 400,
-          color: Colors.red,
-        ),
-        Container(
-          width: 300,
-          height: 300,
-          color: Colors.blue,
-        ),
         Align(
-          alignment: Alignment(0.5, 0.5),
+          alignment: Alignment.center,
           child: Container(
-            width: 200,
-            height: 200,
-            color: Colors.green,
+            width: 300,
+            height: 300,
+            decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(150),
+            ),
           ),
         ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            width: 280,
+            height: 280,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(140),
+            ),
+          ),
+        ),
+        const Align(
+          alignment: Alignment.center,
+          child: Text(
+            'Count 0',
+            style: TextStyle(color: Colors.red, fontSize: 32),
+          ),
+        )
       ],
     );
   }

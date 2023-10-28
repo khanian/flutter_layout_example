3330beb87053ce1e9a0c1721d3709f77b5c79067
khanian <khanylee@gmail.com>
2023-10-29 07:59:35 +0900

    Widget을 겹겹히 쌓아 배치하기 init

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,31 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Widget을 비율로 배치하기'),
-      ),
-      body: Body(),
-    ),
+  runApp(const MaterialApp(
+    home: HomeWidget()
   ));
 }
+
+class HomeWidget extends StatelessWidget {
+  const HomeWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Widget을 겹겹히 쌓아 배치하기'),
+      ),
+      body: Body(),
+    );
+  }
+}
+
 
 class Body extends StatelessWidget {
   const Body({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.red,
-          margin: const EdgeInsets.symmetric(vertical: 8),
-        ),
-        Expanded(
-          child: Container(
-            width: 100,
-            height: 100,
-            color: Colors.red,
-            margin: const EdgeInsets.symmetric(vertical: 8),
-            child: SingleChildScrollView(
-              child: Column(
-                children: [
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                  Container(
-                    color: Colors.blue,
-                    height: 50,
-                    width: 50,
-                    margin: const EdgeInsets.symmetric(vertical: 8),
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ),
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.red,
-          margin: const EdgeInsets.symmetric(vertical: 8),
-        ),
-        Container(
-          width: 100,
-          height: 100,
-          color: Colors.red,
-          margin: const EdgeInsets.symmetric(vertical: 8),
-        ),
-      ],
-    );
+    return Placeholder();
   }
 }

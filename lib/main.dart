44ed104f7a4dbcca472bdbd26c3f2203664bd612
khanian <khanylee@gmail.com>
2023-10-29 06:20:36 +0900

    위젯을 비율로 배치하기 Flexible column

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,13 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Flexible(flex: 1, child: Container(color: Colors.red,)),
-        Flexible(flex: 2, child: Container(color: Colors.blue,)),
-        Flexible(flex: 3, child: Container(color: Colors.green,)),
-        Flexible(flex: 4, child: Container(color: Colors.yellow,)),
+        Container(width: double.infinity, height: 200, color: Colors.red,),
+        Expanded(child: Container(color: Colors.blue, height: 100,)),
+        Flexible(child: Container(color: Colors.red, height: 100,))
+        // Flexible(flex: 1, child: Container(color: Colors.red,)),
+        // Flexible(flex: 2, child: Container(color: Colors.blue,)),
+        // Flexible(flex: 3, child: Container(color: Colors.green,)),
+        // Flexible(flex: 4, child: Container(color: Colors.yellow,)),
       ],
     );
   }

492ccea5a7d78acdc3237d0646235846e8ebabfb
khanian <khanylee@gmail.com>
2023-10-28 15:39:11 +0900

    children test

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Widget을 상하로 배치하기'),
-        ),
-        body: Body(),
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('Widget을 상하로 배치하기'),
       ),
-    )
-  );
+      body: Body(),
+    ),
+  ));
 }
 
 class Body extends StatelessWidget {
@@ -19,6 +17,28 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    // return const Placeholder();
+    return Column(
+      children: [
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.red,
+          child: Text('Contaner 1'),
+        ),
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.green,
+          child: Text('Contaner 2'),
+        ),
+        Container(
+          width: 100,
+          height: 80,
+          color: Colors.blue,
+          child: Text('Contaner 3'),
+        ),
+      ],
+    );
   }
 }

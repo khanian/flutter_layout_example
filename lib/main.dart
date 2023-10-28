3b0ca16d865dc21b74463842bf1d145b55aa94f5
khanian <khanylee@gmail.com>
2023-10-29 08:44:07 +0900

    Constraints init

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: HomeWidget()));
+  runApp(
+      const MaterialApp(
+          home: HomeWidget()
+      )
+  );
 }
 
 class HomeWidget extends StatelessWidget {
@@ -9,52 +13,20 @@ class HomeWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Widget을 겹겹히 쌓아 배치하기'),
-      ),
-      body: Body(),
+    return const SafeArea(
+      child: Scaffold(
+        body: ConstraintWidget(),
+      )
     );
   }
 }
-
-class Body extends StatelessWidget {
-  const Body({super.key});
+class ConstraintWidget extends StatelessWidget {
+  const ConstraintWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: 300,
-            height: 300,
-            decoration: BoxDecoration(
-              color: Colors.red,
-              borderRadius: BorderRadius.circular(150),
-            ),
-          ),
-        ),
-        Align(
-          alignment: Alignment.center,
-          child: Container(
-            width: 280,
-            height: 280,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(140),
-            ),
-          ),
-        ),
-        const Align(
-          alignment: Alignment.center,
-          child: Text(
-            'Count 0',
-            style: TextStyle(color: Colors.red, fontSize: 32),
-          ),
-        )
-      ],
+    return Container(
+      color: Colors.blue,
     );
   }
 }
